3a42d9a0ee699e32fe10cb9ec6a8d45d1a8a97c6
Kunal Bagaria <kb24x7@gmail.com>
2022-01-17 23:00:53 +0530

    feat: added random gradients and default og image on error

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ const { createConverter } = require('convert-svg-to-png');
 const { OgSVG } = require('./svg');
 const imageDataURI = require('image-data-uri');
 const express = require('express')
+const fs = require('fs')
+const path = require('path')
 
 const app = express();
 
@@ -27,10 +29,26 @@ app.get('/:name/:link/:image', async (req: any, res: any) => {
         res.set('Content-Type', 'image/png');
         res.send(png);
     } catch (e) {
-        console.error(e)
-        res.status(500).send(e);
+		console.error(e)
+        res.set('Content-Type', 'image/png');
+		const defaultOG = fs.readFileSync(path.join(__dirname, '..', 'static', 'meta.png'))
+		res.send(defaultOG)
     }
 });
+
+app.get('/random/gradient', async (req: any, res: any) => {
+	try {
+		const directory = path.join(__dirname, '..', 'static', 'random-gradients')
+		const files = fs.readdirSync(directory)
+		const randomFile = files[Math.floor(Math.random() * files.length)]
+		const randomGradient = fs.readFileSync(path.join(directory, randomFile))
+		res.set('Content-Type', 'image/png');
+		res.send(randomGradient);
+	} catch (e) {
+		console.error(e)
+		res.status(500).send(e);
+	}
+})
 
 const port = process.env.PORT || 3000;
 
